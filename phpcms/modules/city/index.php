73ec4cdde21824c247537bf8232b5bf863e2c46f
Wu Zhi Jie <wzj_12@163.com>
2014-01-17 15:33:35 +0800

    add member_list.html

--- phpcms/modules/city/index.php
+++ phpcms/modules/city/index.php
@@ -2,18 +2,15 @@
     defined('IN_PHPCMS') or exit('No permission resources.');
     class index {
         function __construct() {
-            
         }
         public function init()
         {
-            $myvar = 'hello world';
-            echo $myvar;
+            showmessage('正在转向首页...','index.php');
         }
         
         public function mylist()
         {
-            $myvar = 'hello world!this is a example!';
-            echo $myvar;
+      		include template('content','member_list');
         }
-    }
+  }
 ?>
