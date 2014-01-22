56a783653ee83ecd42268c974ef5e62876c9ddc4
Wu Zhi Jie <wzj_12@163.com>
2014-01-22 15:19:07 +0800

    修改加入收藏

--- statics/71zls/js/custom.js
+++ statics/71zls/js/custom.js
@@ -1,2 +1,16 @@
-// 注册页
+// 加入收藏
 
+function addFavorite(){
+        if (document.all){
+                try{
+                        window.external.addFavorite(window.location.href,document.title);
+                }catch(e){
+                        alert( "加入收藏失败，请使用Ctrl+D进行添加" );
+                }
+        
+        }else if (window.sidebar){
+                window.sidebar.addPanel(document.title, window.location.href, "");
+        }else{
+                alert( "加入收藏失败，请使用Ctrl+D进行添加" );
+        }
+}
