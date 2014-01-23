153bdbf496399e7bb6a195f66ea6180f67bf02cf
Wu Zhi Jie <wzj_12@163.com>
2014-01-23 11:42:54 +0800

    做了留言板

--- phpcms/modules/content/MY_index.php
+++ phpcms/modules/content/MY_index.php
@@ -3,6 +3,7 @@ defined('IN_PHPCMS') or exit('No permission resources.');
 //模型缓存路径
 define('CACHE_MODEL_PATH',CACHE_PATH.'caches_model'.DIRECTORY_SEPARATOR.'caches_data'.DIRECTORY_SEPARATOR);
 pc_base::load_app_func('util','content');
+pc_base::load_sys_class('form', '', 0);
 class MY_index extends index{
 	private $db;
 	function __construct() {
