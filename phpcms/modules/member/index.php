2a23aa0b87e0760ff2c1c29ae4b1b4b9895e81f7
zhanhongchen <zhanhongchen@sian.com>
2014-02-08 16:34:36 +0800

    aa

--- phpcms/modules/member/index.php
+++ phpcms/modules/member/index.php
@@ -1192,7 +1192,7 @@ class index extends foreground {
                         $forward = isset($_GET['forward']) && trim($_GET['forward']) ? urlencode($_GET['forward']) : '';
                         $o = new SaeTOAuthV2(WB_AKEY, WB_SKEY);
                         $aurl = $o->getAuthorizeURL(APP_PATH.'index.php?m=member&c=index&a=public_sina_login&callback=1&forward='.$forward);                        
-                        header('Location: '.$aurl);
+                        //header('Location: '.$aurl);
                         //include template('member', 'connect_sina');
                 }
         }
@@ -1864,7 +1864,7 @@ class index extends foreground {
                                 } else {
                                 //如果不需要邮箱认证、直接登录其他应用
                                 $synloginstr = $this->client->ps_member_synlogin($userinfo['phpssouid']);
-                                //showmessage(L('operation_success').$synloginstr, APP_PATH.'index.php?m=member&c=index&a=init');
+                                showmessage(L('operation_success').$synloginstr, APP_PATH.'index.php?m=member&c=index&a=init');
                                 }
                                 
                         }
